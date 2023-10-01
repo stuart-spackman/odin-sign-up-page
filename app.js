094f3fb125d09a8c3b26e911ec38f255aa288535
Stuart Spackman <stuart.spackman@gmail.com>
2023-09-30 19:03:15 -0700

    updates

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const resolved = document.getElementById('password-check');
 
 button.addEventListener('click', (e) => {
     e.preventDefault();
-    if (password.value === confirmPassword.value) {
+    if (password.value === '' || password.value === '') {
+        resolved.classList = [];
+        resolved.classList.add('rejected');
+        resolved.innerText = 'password fields cannot be blank';
+    } else if (password.value === confirmPassword.value) {
         resolved.classList = [];
         resolved.classList.add('resolved');
-        resolved.innerText = 'passwords match';
+        resolved.innerText = 'passwords match - now your illness is cured!';
     } else {
         resolved.classList = [];
         resolved.classList.add('rejected');
